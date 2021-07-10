f8092f8f278ba7a6de52da8c562edf2e2a4c5c33
Devkriops <34610640+Devkriops@users.noreply.github.com>
2021-07-09 22:54:02 -0700

    Update testing.rb

--- testing.rb
+++ testing.rb
@@ -1,1 +1,16 @@
-hi
+#TWA (Prod):
+Start-Process "\\twa.fdc.ibm\shares\Packages\CrowdStrike\Windows\WindowsSensor.GovLaggar.exe" -ArgumentList "/install","/quiet","norestart","CID=9CA52C24495C4E7A99ABB9310BE7AAA2-96","APP_PROXYNAME=10.3.6.30","APP_PROXYPORT=8080","ProvNoWait=1"
+
+#TWA-LLZ (Prod):
+Start-Process "\\twa.fdc.ibm\shares\Packages\CrowdStrike\Windows\WindowsSensor.GovLaggar.exe" -ArgumentList "/install","/quiet","norestart","CID=9CA52C24495C4E7A99ABB9310BE7AAA2-96","APP_PROXYNAME=10.3.33.30","APP_PROXYPORT=8080","ProvNoWait=1"
+
+#TRW (Prod)
+Start-Process "\\triwest.com\shares\Packages\CrowdStrike\Windows\WindowsSensor.GovLaggar.exe" -ArgumentList "/install","/quiet","norestart","CID=9CA52C24495C4E7A99ABB9310BE7AAA2-96","APP_PROXYNAME=10.3.6.30","APP_PROXYPORT=8080","ProvNoWait=1"
+
+#TWS (Prod)
+Start-Process "\\triwest.com\shares\Packages\CrowdStrike\Windows\WindowsSensor.GovLaggar.exe" -ArgumentList "/install","/quiet","norestart","CID=9CA52C24495C4E7A99ABB9310BE7AAA2-96","APP_PROXYNAME=10.3.109.30","APP_PROXYPORT=8080","ProvNoWait=1"
+
+#TWS (Non-Prod)
+Start-Process "\\triwest.com\shares\Packages\CrowdStrike\Windows\WindowsSensor.GovLaggar.exe" -ArgumentList "/install","/quiet","norestart","CID=9CA52C24495C4E7A99ABB9310BE7AAA2-96","APP_PROXYNAME=10.4.2.30","APP_PROXYPORT=8080","ProvNoWait=1"
+
+
