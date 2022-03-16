65fd1d95f06f3ca73a33393711610fc301699d10
Devkriops <34610640+Devkriops@users.noreply.github.com>
2022-03-16 00:11:04 -0700

    Update hostlogic.py

--- hostlogic.py
+++ hostlogic.py
@@ -1,26 +1,26 @@
-#!/usr/bin/python
-
-username = input('enter username: ')
-filepath = input('enter filepath: ')
-
 import os
+count = 0
 
+username = input("Enter username: ")
+file_path = input("Enter filepath: ")
 
-file1 = open(filepath, "r")
+try:
+    with open(file_path, 'r') as my_file:
 
-while(True):
-        #read next line
-        line = file1.readline()
-        #check if line is not null
-        if not line:
-                break
-        #you can access the line
-        print(line.strip())
-        hostname = line.strip()
-        # stream = os.popen(f'ssh {username}@{hostname} copy ./test.sh /tmp/test.sh')
-        # stream = os.popen(f'ssh {username}@{hostname} ./test.sh')
-        stream = os.popen(f'ls')
-        output = stream.readlines()
-        print output
-#close file
-file1.close
+        # Using for loop
+        for line in my_file:
+            count += 1
+            host = line.strip()
+            print("Line{}: {}".format(count, host))
+           
+            stream = os.popen('ls -la')
+            # stream = os.popen(f'ssh {username}@{hostname} copy ./test.sh /tmp/test.sh')
+            # stream = os.popen(f'ssh {username}@{hostname} ./test.sh')
+            output = stream.readlines()
+            print(output)
+
+except Exception as ex:
+    print("*******")
+    print(str(ex))
+    print("*******")
+    raise ex
