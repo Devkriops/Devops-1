e68efdee5e2be7840a0734979b33ca57195687a2
Devkriops <34610640+Devkriops@users.noreply.github.com>
2022-03-15 23:34:40 -0700

    Create hostlogic.py

---
+++ hostlogic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+username = input('enter username: ')
+filepath = input('enter filepath: ')
+
+import os
+
+
+file1 = open("/home/scguser/hostlist", "r")
+
+while(True):
+        #read next line
+        line = file1.readline()
+        #check if line is not null
+        if not line:
+                break
+        #you can access the line
+        print(line.strip())
+        hostname = line.strip()
+        # stream = os.popen(f'ssh {username}@{hostname} copy ./test.sh /tmp/test.sh')
+        # stream = os.popen(f'ssh {username}@{hostname} ./test.sh')
+        stream = os.popen(f'ls')
+        output = stream.readlines()
+        print output
+#close file
+file1.close
