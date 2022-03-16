6401287866e61a7b0562c2b675a9eb46598d5dbb
Devkriops <34610640+Devkriops@users.noreply.github.com>
2022-03-16 15:44:26 -0700

    Update hostlogic.py

--- hostlogic.py
+++ hostlogic.py
@@ -24,3 +24,34 @@ except Exception as ex:
     print(str(ex))
     print("*******")
     raise ex
+---------------------------------------------------------------
+
+
+#!/usr/bin/python
+
+username = input('enter username: ')
+filepath = input('enter filepath: ')
+
+import os
+
+
+file1 = open(filepath, "r")
+
+while(True):
+        #read next line
+        line = file1.readline()
+        #check if line is not null
+        if not line:
+                break
+        #you can access the line
+        print(line.strip())
+        hostname,tag = line.strip().split(',')
+        # stream = os.popen(f'ssh {username}@{hostname} copy ./test.sh /tmp/test.sh')
+        # stream = os.popen(f'ssh {username}@{hostname} ./test.sh')
+        #stream = os.popen(f'ls')
+        #output = stream.readlines()
+        print output
+        print hostname
+        print tag
+#close file
+file1.close
