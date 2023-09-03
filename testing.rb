bbf6126e22545bb040ebf9fe01479b8b1a8dc26b
Devkriops <34610640+Devkriops@users.noreply.github.com>
2023-09-02 17:23:48 -0700

    Update testing.rb

--- testing.rb
+++ testing.rb
@@ -1,1 +1,20 @@
-hi
+require 'wmi-lite'
+
+# Connect to the WMI namespace
+wmi = WmiLite::Wmi.new
+
+# Query for Win32_CDROMDrive class to find DVD drives
+dvd_drives = wmi.query('SELECT * FROM Win32_CDROMDrive')
+
+if dvd_drives.empty?
+  puts 'No DVD drives found.'
+else
+  puts 'DVD Drives:'
+  dvd_drives.each do |drive|
+    puts "Device ID: #{drive['DeviceID']}"
+    puts "Drive Name: #{drive['Name']}"
+    puts "Drive Manufacturer: #{drive['Manufacturer']}"
+    puts "Drive Description: #{drive['Description']}"
+    puts
+  end
+end
