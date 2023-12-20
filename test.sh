89d7099e19d863dffa2064c319fa7e4506f7e783
Devkriops <34610640+Devkriops@users.noreply.github.com>
2023-12-20 13:14:13 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,3 +10,12 @@ zero_value=$(awk -F'</?div>' '/<td align="center">/{getline; print $2}' "$html_f
 # Print the results
 echo "High Value: $high_value"
 echo "Zero Value: $zero_value"
+------------------------------------
+
+#!/bin/bash
+
+# Specify the path to your HTML file
+html_file="your_file.html"
+
+# Use awk to extract values associated with "risk-3" and "High"
+awk -F'</?div>' '/<td class="risk-3">/{getline; risk=$2} /<td align="center">/{getline; print "Risk Level: " risk ", Value: " $2}' "$html_file"
