fcc10793f1ffc2c93c962d0062ccfe9096d07e83
Devkriops <34610640+Devkriops@users.noreply.github.com>
2023-12-20 13:10:05 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,11 +3,9 @@
 # Specify the path to your HTML file
 html_file="your_file.html"
 
-# Extract the value associated with "High"
-high_value=$(grep -oP '<td class="risk-3">\s*<div>\K[^<]+' "$html_file")
-
-# Extract the value associated with "0"
-zero_value=$(grep -oP '<td align="center">\s*<div>\K[^<]+' "$html_file")
+# Use awk to extract values associated with "High" and "0"
+high_value=$(awk -F'</?div>' '/<td class="risk-3">/{getline; print $2}' "$html_file")
+zero_value=$(awk -F'</?div>' '/<td align="center">/{getline; print $2}' "$html_file")
 
 # Print the results
 echo "High Value: $high_value"
