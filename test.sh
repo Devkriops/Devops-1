f29655bcaf1abb77ffcfb8ce3d7cd75c6c19618c
Devkriops <34610640+Devkriops@users.noreply.github.com>
2023-12-20 13:05:17 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,14 @@
-Data: file path
-# Open the file for reading (change 'dates.txt' to your file's name)
-with open('dates.txt', 'r') as file:
-    # Loop through each line in the file
-    for line in file:
-        # Remove leading and trailing whitespace from the line
-        line = line.strip()
-        
-        # Process the raw date string (You can replace this part with your own logic)
-        print(f"Raw Date: {line}")
-        
-        # Add your date parsing and processing logic here
-        # For example, you can use date parsing libraries like datetime to work with dates
+#!/bin/bash
 
-        # For illustration, let's assume we want to split the date string by '-' and print the parts
-        date_parts = line.split('-')
-        print(f"Year: {date_parts[0]}, Month: {date_parts[1]}, Day: {date_parts[2]}\n")
+# Specify the path to your HTML file
+html_file="your_file.html"
+
+# Extract the value associated with "High"
+high_value=$(grep -oP '<td class="risk-3">\s*<div>\K[^<]+' "$html_file")
+
+# Extract the value associated with "0"
+zero_value=$(grep -oP '<td align="center">\s*<div>\K[^<]+' "$html_file")
+
+# Print the results
+echo "High Value: $high_value"
+echo "Zero Value: $zero_value"
