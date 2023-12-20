971f6415b3a7a7faf826d7b1f253d8c2eedac0bf
Devkriops <34610640+Devkriops@users.noreply.github.com>
2023-12-20 13:53:50 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,13 +3,30 @@
 # Specify the path to your HTML file
 html_file="your_file.html"
 
-# Use awk to extract values associated with "High" and "0"
-high_value=$(awk -F'</?div>' '/<td class="risk-3">/{getline; print $2}' "$html_file")
-zero_value=$(awk -F'</?div>' '/<td align="center">/{getline; print $2}' "$html_file")
+# Use awk to extract value associated with "risk-3" and "High"
+awk -F'</?div>' '/<td class="risk-3">/{getline; risk_value=$2; found_risk=1} /<td align="center">/{if(found_risk) {getline; center_value=$2; exit}} END {if (found_risk) print center_value}' "$html_file" | while read -r risk3_value; do
+    quality_gate="yes"  # Set this variable based on your criteria
+    
+    if [[ "$quality_gate" == "yes" ]]; then
+        if [[ "$risk3_value" == "0" ]]; then
+            echo "Quality Gate Parameter: Yes"
+            echo "Risk-3 Value: $risk3_value"
+            echo "Success"
+        else
+            echo "Quality Gate Parameter: Yes"
+            echo "Risk-3 Value: $risk3_value"
+            echo "Failed"
+            exit 1  # Exit the script with an error code
+        fi
+    elif [[ "$quality_gate" == "nothing" ]]; then
+        echo "Quality Gate Parameter: Nothing"
+        echo "Risk-3 Value: $risk3_value"
+    else
+        echo "Invalid quality gate parameter"
+        exit 1  # Exit the script with an error code
+    fi
+done
 
-# Print the results
-echo "High Value: $high_value"
-echo "Zero Value: $zero_value"
 ------------------------------------
 
 #!/bin/bash
