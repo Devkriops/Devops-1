6231a82040bca19316e3c1d141dae3f10c833a73
Devkriops <34610640+Devkriops@users.noreply.github.com>
2023-12-22 13:27:41 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -37,3 +37,39 @@ html_file="your_file.html"
 # Use awk to extract value associated with "risk-3" and "High"
 awk -F'</?div>' '/<td class="risk-3">/{getline; risk_value=$2; found_risk=1} /<td align="center">/{if(found_risk) {getline; center_value=$2; print "Risk-3 Value: " center_value; exit}}' "$html_file"
 ------------------------------------
+FROM registry.redhat.io/ubi8/ubi-minimal:8.4
+
+# Install required packages
+RUN microdnf install -y openssh-server openssh-clients openssl
+
+# Download and install OpenSSH 8.1
+ADD https://openbsd.cs.toronto.edu/pub/OpenBSD/OpenSSH/portable/openssh-8.1p1.tar.gz /tmp
+WORKDIR /tmp
+RUN tar -xzvf openssh-8.1p1.tar.gz && \
+    cd openssh-8.1p1 && \
+    ./configure && \
+    make && \
+    make install && \
+    cd /tmp && \
+    rm -rf openssh-8.1p1 openssh-8.1p1.tar.gz
+
+# Download and install OpenSSL 3.0.7
+ADD https://www.openssl.org/source/openssl-3.0.7.tar.gz /tmp
+WORKDIR /tmp
+RUN tar -xzvf openssl-3.0.7.tar.gz && \
+    cd openssl-3.0.7 && \
+    ./config && \
+    make && \
+    make install && \
+    cd /tmp && \
+    rm -rf openssl-3.0.7 openssl-3.0.7.tar.gz
+
+# Cleanup
+RUN microdnf clean all
+
+# Configure SSH
+RUN mkdir /var/run/sshd
+RUN ssh-keygen -A
+
+# Start SSH server
+CMD ["/usr/sbin/sshd", "-D"]
